f4a8c29d4d94b46c12c818b0951a938fb7bca06c
Andrew Bwogi <Andrew.bwogi@gmail.com>
2021-10-22 12:25:46 -0700

    a number of fixes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>
-      portfolio
+    <div className="modalBackdrop">
+      <div className="modalContainer">
+        <h3 className="modalTitle">Photo Name</h3>
+        <img alt="current category" />
+        <p>
+          Photo Description
+        </p>
+        <button type="button">
+          Close this modal
+        </button>
+      </div>
     </div>
   )
 }
