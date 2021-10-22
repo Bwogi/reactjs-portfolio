4fabc442f5a87b99576a7562ac9c6796ac00d540
Andrew Bwogi <Andrew.bwogi@gmail.com>
2021-10-22 12:31:03 -0700

    Updates

---
+++ precache-manifest.d2737bc828418a0f0354232d1eb93978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52a90f2a2f47a17c2400",
+    "url": "/reactjs-portfolio/static/css/main.31124b92.chunk.css"
+  },
+  {
+    "revision": "52a90f2a2f47a17c2400",
+    "url": "/reactjs-portfolio/static/js/main.52a90f2a.chunk.js"
+  },
+  {
+    "revision": "60ee61b78af30bd895fc",
+    "url": "/reactjs-portfolio/static/js/1.60ee61b7.chunk.js"
+  },
+  {
+    "revision": "81bc7008912689f902e4",
+    "url": "/reactjs-portfolio/static/js/runtime~main.81bc7008.js"
+  },
+  {
+    "revision": "fc6aeb7cba791446b03762fc64cfc39e",
+    "url": "/reactjs-portfolio/static/media/Resume.fc6aeb7c.pdf"
+  },
+  {
+    "revision": "0bdbe3b7eaf6a91481775456bf643d87",
+    "url": "/reactjs-portfolio/index.html"
+  }
+];
